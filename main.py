7e18b82a6e91235ec5fbc87897e778ca51e3ffea
Jeff Morice <jmorice.anim@gmail.com>
2018-10-20 01:01:21 -0500

    Implemented signup functionality with user data validation. Modified login.html for display preview purposes. Ported some styles from user-sign up to styles.css.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import desc
+import string
 
 # define your app
 app = Flask(__name__)
@@ -35,7 +36,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), unique=True)
     password = db.Column(db.String(32))
-    email = db.Column(db.String(120), unique=True)
+    email = db.Column(db.String(120), unique=True)       # change to accept emails of up to 254 characters in length
     blogs = db.relationship('Blog', backref='owner')
 
     def __init__(self, username, password, email):
@@ -43,13 +44,145 @@ class User(db.Model):
         self.password = password
         self.email = email
 
+def validate_username():
+    username_error = ''                            # {0}
+
+    # retrieve form data
+    u_candidate = request.form["username"]         # {4}
+
+    # username validation
+    if len(u_candidate) == 0:
+        username_error = 'Please enter a username.'
+    elif ' ' in u_candidate:
+        username_error = 'cannot contain spaces.'
+    elif len(u_candidate) < 3 or len(u_candidate) > 20:
+        username_error = 'must be between 3 and 20 characters.'
+    elif u_candidate.isalnum() == False:
+        username_error = 'cannot contain special characters.'
+
+    return [u_candidate, username_error]
+
+def validate_password():
+    # password validation
+    password_error = ''                            # {1}
+    p_verification_error = ''                      # {2}
+
+    # retrieve form data
+    u_candidate = request.form["username"]
+    p_candidate = request.form["password"]
+    p_verified = request.form["password_verified"]
+
+    if len(p_candidate) == 0:
+        password_error = 'Please enter a password.'
+        p_candidate = ''
+        p_verified = ''
+    elif ' ' in p_candidate:
+        password_error = 'cannot contain spaces.'
+        p_candidate = ''
+        p_verified = ''
+    elif len(p_candidate) < 8 or len(p_candidate) > 20:
+        password_error = 'must be between 8 and 20 characters.'
+        p_candidate = ''
+        p_verified = ''
+    elif u_candidate in p_candidate or p_candidate in u_candidate:
+        password_error = 'should not be similar to usernames.'
+        p_candidate = ''
+        p_verified = ''
+    # password matching validation
+    if p_candidate != p_verified:
+        p_verification_error = 'Passwords do not match.'
+        p_candidate = ''
+        p_verified = ''
+
+    return [p_candidate, password_error, p_verification_error]
+
+def validate_email():
+    # email validation
+    email_error = ''                               # {3}
+
+    # retrieve form data
+    e_candidate = request.form["email"]            # {5}
+
+    if e_candidate == '':
+        email_error = ''
+    elif ' ' in e_candidate:
+        email_error = 'cannot contain spaces.'
+    elif len(e_candidate) < 3 or len(e_candidate) > 254:
+        email_error = 'must be between 3 and 254 characters.'
+    elif e_candidate.count("@") != 1:
+        email_error = 'must contain 1 "@" symbol.'
+    elif e_candidate.count(".") < 1:
+        email_error = 'must contain at least 1 "." symbol.'
+
+    return [e_candidate, email_error]
+
+def signup_validation():
+    u_validation = validate_username()
+    p_validation = validate_password()
+    e_validation = validate_email()
+
+    u_candidate = u_validation[0]
+    p_candidate = p_validation[0]
+    e_candidate = e_validation[0]
+
+    u_error = u_validation[1]
+    p_error = p_validation[1]
+    pv_error = p_validation[2]
+    e_error = e_validation[1]
+
+    u_confirmed = ''
+    p_confirmed = ''
+    e_confirmed = ''
+
+    valid = True
+
+    # validate username
+    if len(u_error) == 0:
+        u_confirmed = u_candidate
+    else:
+        valid = False
+
+    # validate password
+    if len(p_error + pv_error) == 0:
+        p_confirmed = p_candidate
+    else:
+        valid = False
+
+    # validate email
+    if len(e_error) == 0:
+        e_confirmed = e_candidate
+    else:
+        valid = False
+
+    # if valid, pass confirmed variables
+    # if not valid, pass candidates with errors
+    if valid == True:
+        return [u_confirmed, p_confirmed, e_confirmed]
+    else:
+        return [u_error, p_error, pv_error, e_error, u_candidate, e_candidate]
+
+    # # return errors or redirect if all input is valid
+    # if len(username_error + password_error + email_error + p_verification_error) == 0:
+    #     #if all input is valid, redirect
+    #     u_confirmed = u_candidate
+    #     return redirect("/welcome?user=" + u_confirmed)
+    # else:
+    #     return render_template('signup.html',
+    #     username_error=username_error,
+    #     password_error=password_error,
+    #     p_verification_error=p_verification_error,
+    #     email_error=email_error,
+    #     u_candidate=u_candidate,
+    #     e_candidate=e_candidate)
+
 # define your request handlers, one for each page
     # include any logic, say for validation or updating the database
     # return rendered templates or redirect. Don't forget to return!
 @app.route('/newpost', methods=['POST', 'GET'])
 def newpost():
 
-    owner = User.query.filter_by(id=session['user_id']).first()
+    # assigns whole object with matching username
+    owner = User.query.filter_by(username=session['username']).first()
 
     if request.method == 'POST':
         entry_title = request.form['entry_title']
@@ -124,9 +257,55 @@ def delete_entry():
 def index():
     return 'index page'
 
-@app.route('/signup')
+@app.route('/signup', methods=['POST', 'GET'])
 def signup():
+    if request.method == 'POST':
+        user_data = signup_validation()
+
+        if len(user_data) == 3:
+            # user data passed validation
+            username = user_data[0]
+            password = user_data[1]
+            email = user_data[2]
+
+            # Check if user already exists
+            existing_username = User.query.filter_by(username=username).first()
+            existing_email = User.query.filter_by(email=email).first()
+            if not existing_username and not existing_email:
+                new_user = User(username, password, email)
+                db.session.add(new_user)
+                db.session.commit()
+                session['username'] = username
+                return redirect('/newpost')
+            else:
+                username_error = ''
+                email_error = ''
+
+                if existing_username:
+                    username_error = 'Username already exists.'
+                if existing_email:
+                    email_error = 'Email Address already exists.'
+
+                return render_template('signup.html',
+                username_error=username_error,
+                password_error='',
+                p_verification_error='',
+                email_error=email_error,
+                u_candidate=username,
+                e_candidate=email)
+
+        elif len(user_data) == 6:
+            # user data failed validation
+            return render_template('signup.html',
+            username_error=user_data[0],
+            password_error=user_data[1],
+            p_verification_error=user_data[2],
+            email_error=user_data[3],
+            u_candidate=user_data[4],
+            e_candidate=user_data[5])
+
     return render_template('signup.html')
+
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
@@ -135,9 +314,9 @@ def login():
         password = request.form['password']
         user = User.query.filter_by(username=username).first()
         if user and user.password == password:
-            session['user_id'] = user.id
+            session['username'] = username
             flash("Logged in")
-            return redirect('/')
+            return redirect('/newpost')
         else:
             flash('User password incorrect, or user does not exist', 'error')
             return render_template('login.html', username=username)
