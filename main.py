c4e6c6a4996690088762f4c2ace57cce9a448195
Jeff Morice <jmorice.anim@gmail.com>
2018-10-19 22:42:24 -0500

    Added a User class. Refactored Blog class to keep track of its owner. Added login logic. Added a session containing user_id. Changed 'Blog' link in base.html to link to /blog route rather than index. Added check for 0 entries to blog.html. Modified login.html to accept a username rather than an email.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # import relevant modules
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import desc
 
@@ -12,26 +12,44 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:xylophone@localho
 app.config['SQLALCHEMY_ECHO'] = True
 # create a reference to database and its methods
 db = SQLAlchemy(app)
+app.secret_key = 'bigglesdoingthewiggle'
 
 # define any classes - used to construct objects, usually an entry in a table.
-# So far, we've only used one class per table: User, Task
+# So far, we've only used one class per table: User, Blog
 class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.Text)
     visible = db.Column(db.Boolean)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, body, visible):
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
         self.visible = True
+        self.owner = owner
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), unique=True)
+    password = db.Column(db.String(32))
+    email = db.Column(db.String(120), unique=True)
+    blogs = db.relationship('Blog', backref='owner')
+
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
 
 # define your request handlers, one for each page
     # include any logic, say for validation or updating the database
     # return rendered templates or redirect. Don't forget to return!
 @app.route('/newpost', methods=['POST', 'GET'])
 def newpost():
+
+    owner = User.query.filter_by(id=session['user_id']).first()
 
     if request.method == 'POST':
         entry_title = request.form['entry_title']
@@ -52,7 +70,7 @@ def newpost():
             title_error=title_error,
             body_error=body_error)
         else:
-            new_entry = Blog(entry_title, entry_body, True)
+            new_entry = Blog(entry_title, entry_body, owner)
             db.session.add(new_entry)
             db.session.commit()
 
@@ -62,12 +80,13 @@ def newpost():
     return render_template('newpost.html')
 
 
-@app.route('/')
-def index():
+@app.route('/blog')
+def blog():
     # assigns a list of objects of class 'Blog' to 'entries' variable
         # took way to long to figure out. Apparently you have to order a select
         # before you filter it? No idea. Could swear I tried that.
     entries = Blog.query.order_by(desc(Blog.id)).filter_by(visible=True).all()
+    # entries = []
 
     return render_template('blog.html',
     title="Your Blog Name Here!",
@@ -100,6 +119,30 @@ def delete_entry():
     db.session.commit()
 
     return redirect('/blog')
+
+@app.route('/')
+def index():
+    return 'index page'
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            session['user_id'] = user.id
+            flash("Logged in")
+            return redirect('/')
+        else:
+            flash('User password incorrect, or user does not exist', 'error')
+            return render_template('login.html', username=username)
+
+    return render_template('login.html')
 
 # don't forget to run the fucking app (only if __name__ == "__main__")
 if __name__ == '__main__':
